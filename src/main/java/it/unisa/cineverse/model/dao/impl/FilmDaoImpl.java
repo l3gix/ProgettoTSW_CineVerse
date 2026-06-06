e368727eec59a5eda10f0e6fb5d025a4c377123f
michele <michele.milione12@gmail.com>
2026-06-06 19:07:10 +0200

    Implementazione classi DAO

--- src/main/java/it/unisa/cineverse/model/dao/impl/FilmDaoImpl.java
+++ src/main/java/it/unisa/cineverse/model/dao/impl/FilmDaoImpl.java
@@ -1,5 +1,126 @@
 package it.unisa.cineverse.model.dao.impl;
 
-public class FilmDaoImpl {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import it.unisa.cineverse.model.bean.CategoriaPostiBean;
+import it.unisa.cineverse.model.bean.FilmBean;
+import it.unisa.cineverse.model.dao.FilmDao;
+
+public class FilmDaoImpl implements FilmDao{
+	private static final String TABLE_NAME ="film "; 
+	private DataSource ds= null;
+	
+	public FilmDaoImpl(DataSource ds) {
+		this.ds=ds;
+	}
+
+	@Override
+	public synchronized void save(FilmBean film) throws SQLException {
+		String sql= "INSERT INTO" +TABLE_NAME +"(titolo, sinossi, durata_minuti, age_rating, data_rilascio, trailer_url, cast_film, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+		try(Connection connection=ds.getConnection();
+				PreparedStatement ps= connection.prepareStatement(sql)){
+			ps.setString(1, film.getTitolo());
+			ps.setString(2, film.getSinossi());
+			ps.setInt(3, film.getDurata_minuti());
+			ps.setString(4, film.getAge_rating());
+			ps.setTimestamp(5, Timestamp.valueOf(film.getData_rilascio()));
+			ps.setString(6, film.getTrailer_url());
+			ps.setString(7, film.getCast_film());
+			ps.setString(8, film.getStatus());
+			ps.executeUpdate();
+		}
+	}
+
+	@Override
+	public synchronized void update(FilmBean film) throws SQLException {
+		String sql= "UPDATE" +TABLE_NAME +" SET titolo = ?, sinossi = ?, durata_minuti = ?, age_rating = ?,  data_rilascio = ?, trailer_url = ?, cast_film = ?, status = ? WHERE id = ?";
+		try(Connection connection=ds.getConnection();
+				PreparedStatement ps= connection.prepareStatement(sql)){
+			ps.setString(1, film.getTitolo());
+			ps.setString(2, film.getSinossi());
+			ps.setInt(3, film.getDurata_minuti());
+			ps.setString(4, film.getAge_rating());
+			ps.setTimestamp(5, Timestamp.valueOf(film.getData_rilascio()));
+			ps.setString(6, film.getTrailer_url());
+			ps.setString(7, film.getCast_film());
+			ps.setString(8, film.getStatus());
+			ps.setInt(9, film.getId());
+			ps.executeUpdate();
+		}
+	}
+
+	@Override
+	public synchronized void delete(int id) throws SQLException {
+		
+		String sql= "DELETE FROM" +TABLE_NAME +" WHERE id = ?";
+		try(Connection connection=ds.getConnection();
+				PreparedStatement ps= connection.prepareStatement(sql)){
+			ps.setInt(1, id);
+			ps.executeUpdate();
+		}
+		
+	}
+
+	@Override
+	public synchronized List<FilmBean> findAll() throws SQLException {
+		List<FilmBean> film = new ArrayList<FilmBean>();
+		String sql="SELECT * FROM" +TABLE_NAME;
+		try(Connection connection=ds.getConnection();
+				PreparedStatement ps= connection.prepareStatement(sql);
+				ResultSet rs = ps.executeQuery()){
+			while(rs.next()) {
+				FilmBean bean= new FilmBean();
+				bean.setId(rs.getInt("id"));
+				bean.setTitolo(rs.getString("titolo"));
+				bean.setSinossi(rs.getString("sinossi"));
+				bean.setDurata_minuti(rs.getInt("durata_minuti"));
+				bean.setAge_rating(rs.getString("age_rating"));
+				bean.setData_rilascio(rs.getTimestamp("data_rilascio").toLocalDateTime());
+				bean.setTrailer_url(rs.getString("trailer_url"));
+				bean.setCast_film(rs.getString("cast_film"));
+				bean.setStatus(rs.getString("status"));
+				film.add(bean);
+				}
+		}
+		
+		return film;
+	}
+
+	@Override
+	public synchronized FilmBean findbyId(int id) throws SQLException {
+		FilmBean bean = new FilmBean();
+		String sql= "SELECT * FROM" +TABLE_NAME +" WHERE id = ?";
+		
+		try(Connection connection=ds.getConnection();
+				PreparedStatement ps= connection.prepareStatement(sql);){
+				ps.setInt(1,id);
+				try (ResultSet rs  = ps.executeQuery()){
+					while(rs.next()) {
+						bean.setId(rs.getInt("id"));
+						bean.setTitolo(rs.getString("titolo"));
+						bean.setSinossi(rs.getString("sinossi"));
+						bean.setDurata_minuti(rs.getInt("durata_minuti"));
+						bean.setAge_rating(rs.getString("age_rating"));
+						bean.setData_rilascio(rs.getTimestamp("data_rilascio").toLocalDateTime());
+						bean.setTrailer_url(rs.getString("trailer_url"));
+						bean.setCast_film(rs.getString("cast_film"));
+						bean.setStatus(rs.getString("status"));
+						
+					}
+				}
+		}
+		return bean;
+	
+		
+
+	}
 
 }
--- src/main/java/it/unisa/cineverse/model/dao/impl/FormatoFilmDaoImpl.java
+++ src/main/java/it/unisa/cineverse/model/dao/impl/FormatoFilmDaoImpl.java
@@ -1,5 +1,91 @@
 package it.unisa.cineverse.model.dao.impl;
 
-public class FormatoFilmDaoImpl {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import it.unisa.cineverse.model.bean.FilmBean;
+import it.unisa.cineverse.model.bean.FormatoFilmBean;
+import it.unisa.cineverse.model.dao.FormatoFilmDao;
+
+public class FormatoFilmDaoImpl implements FormatoFilmDao {
+	private static final String TABLE_NAME ="formato_film "; 
+	private DataSource ds= null;
+	
+	public FormatoFilmDaoImpl(DataSource ds) {
+		this.ds= ds;
+	}
+	@Override
+	public synchronized void save(FormatoFilmBean formato) throws SQLException {
+		String sql = "INSERT INTO" +TABLE_NAME +" (name) VALUES (?)";
+		try(Connection connection=ds.getConnection();
+				PreparedStatement ps= connection.prepareStatement(sql)){
+			ps.setString(1, formato.getName());
+			ps.executeUpdate();
+		}
+	}
+
+	@Override
+	public synchronized void update(FormatoFilmBean formato) throws SQLException {
+		String sql = "UPDATE" +TABLE_NAME +"film SET name = ? WHERE id = ?";
+		try(Connection connection=ds.getConnection();
+				PreparedStatement ps= connection.prepareStatement(sql)){
+			ps.setString(1, formato.getName());
+			ps.setInt(2, formato.getId());
+			ps.executeUpdate();			
+		}
+		
+		
+	}
+
+	@Override
+	public synchronized void delete(int id) throws SQLException {
+		String sql= "DELETE FROM" +TABLE_NAME +" WHERE id = ?";
+		try(Connection connection=ds.getConnection();
+				PreparedStatement ps= connection.prepareStatement(sql)){
+			ps.setInt(1, id);
+			ps.executeUpdate();
+		}
+	}
+
+	@Override
+	public synchronized List<FormatoFilmBean> findAll() throws SQLException {
+		List<FormatoFilmBean> formato_film = new ArrayList<FormatoFilmBean>();
+		String sql= "SELECT * FROM " +TABLE_NAME;
+		try(Connection connection=ds.getConnection();
+				PreparedStatement ps= connection.prepareStatement(sql);
+				ResultSet rs = ps.executeQuery()){
+			while(rs.next()) {
+				FormatoFilmBean bean = new FormatoFilmBean();
+				bean.setId(rs.getInt("id"));
+				bean.setName(rs.getString("name"));
+				formato_film.add(bean);
+			}
+		}
+		return formato_film;
+	}
+
+	@Override
+	public synchronized FormatoFilmBean findbyId(int id) throws SQLException {
+		FormatoFilmBean bean = new FormatoFilmBean();
+		String sql=" SELECT * FROM " +TABLE_NAME +" WHERE id = ?";
+		
+		try(Connection connection=ds.getConnection();
+				PreparedStatement ps= connection.prepareStatement(sql);){
+				ps.setInt(1,id);
+				try (ResultSet rs  = ps.executeQuery()){
+					while(rs.next()) {
+						bean.setId(rs.getInt("id"));
+						bean.setName(rs.getString("name"));
+					}
+				}
+		}		
+		return bean;
+	}
 
 }
